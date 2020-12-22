df42c1fbf175829a5adfbd92cd8f939a868e2339
strogiyotec <almas337519@gmail.com>
2020-12-22 11:45:17 -0800

    day 13 part 1

---
+++ day13/part1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type tuple struct {
+	first  int
+	second int
+}
+
+func main() {
+	fmt.Println("HERE")
+	inputFile, _ := ioutil.ReadFile("input")
+	lines := strings.Split(string(inputFile), "\n")
+	earliest, _ := strconv.Atoi(lines[0])
+	busses := []int{}
+
+	ids := strings.Split(string(lines[1]), ",")
+	for _, id := range ids {
+		numId, e := strconv.Atoi(id)
+		if e == nil {
+			busses = append(busses, numId)
+		}
+	}
+	fmt.Println(busses, earliest)
+	smallestNearest := 2147483647
+	busId := 0
+	for _, bus := range busses {
+		wait := ((earliest / bus) * bus) + bus
+		if smallestNearest > wait {
+			smallestNearest = wait
+			busId = bus
+		}
+	}
+	needToWait := (smallestNearest - earliest) * busId
+	fmt.Println(needToWait)
+}
