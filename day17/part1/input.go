db9e54c615b929b452056a50ca0a80da89b20d53
strogiyotec <almas337519@gmail.com>
2021-01-03 14:21:46 -0800

    day 16 and 17

---
+++ day17/part1/input.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var flagInput = flag.String("input", "input.txt", "input file to read from")
+
+func Input() string {
+	flag.Parse()
+
+	fmt.Println("reading", *flagInput)
+
+	buf, err := ioutil.ReadFile(*flagInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(buf)
+}
+
+func InputLines() []string {
+	return strings.Split(strings.Trim(Input(), "\n"), "\n")
+}
+
+func InputParas() [][]string {
+	var res [][]string
+	for _, para := range strings.Split(strings.Trim(Input(), "\n"), "\n\n") {
+		res = append(res, strings.Split(para, "\n"))
+	}
+	return res
+}
+
+func InputInts() []int {
+	return Ints(InputLines())
+}
+
+func Ints(list []string) []int {
+	var res []int
+	for _, n := range list {
+		res = append(res, int(Atoi(n)))
+	}
+	return res
+}
+
+func Atoi(s string) int64 {
+	n, err := strconv.ParseInt(s, 0, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return n
+}
+
+func Sscanf(str, format string, a ...interface{}) {
+	_, err := fmt.Sscanf(str, format, a...)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NextPermutation(data sort.Interface) bool {
+	n := data.Len()
+	for i := n - 2; i >= 0; i-- {
+		if data.Less(i, i+1) {
+			j := n - 1
+			for !data.Less(i, j) {
+				j--
+			}
+			data.Swap(i, j)
+			reverse(data, i+1, n)
+			return true
+		}
+	}
+
+	reverse(data, 0, n)
+	return false
+}
+
+func reverse(data sort.Interface, beg, end int) {
+	end--
+	for beg < end {
+		data.Swap(beg, end)
+
+		beg++
+		end--
+	}
+}
+
+func EachPermutation(data sort.Interface, f func()) {
+	for {
+		f()
+		if !NextPermutation(data) {
+			break
+		}
+	}
+}
---
+++ day17/part1/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+)
+
+const Cycles = 6
+const N = Cycles + 8 + Cycles
+const M = Cycles + 1 + Cycles
+
+// Indexed: [x][y][z]
+var old, new [N][N][M][M]bool
+
+func getOld(x, y, z, w int) bool {
+	if x < 0 || x >= N {
+		return false
+	}
+	if y < 0 || y >= N {
+		return false
+	}
+	if z < 0 || z >= M {
+		return false
+	}
+	if w < 0 || w >= M {
+		return false
+	}
+	return old[x][y][z][w]
+}
+
+func neighbors(x, y, z, w int) int {
+	sum := 0
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			for dz := -1; dz <= 1; dz++ {
+				for dw := -1; dw <= 1; dw++ {
+					if (dx != 0 || dy != 0 || dz != 0 || dw != 0) && getOld(x+dx, y+dy, z+dz, w+dw) {
+						sum++
+					}
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	for y, line := range InputLines() {
+		for x, ch := range line {
+			if ch == '#' {
+				new[Cycles+x][Cycles+y][Cycles][Cycles] = true
+			}
+		}
+	}
+
+	for i := 0; i < Cycles; i++ {
+		old = new
+		for x := range new {
+			for y := range new[x] {
+				for z := range new[x][y] {
+					for w := range new[x][y][z] {
+						n := neighbors(x, y, z, w)
+						if old[x][y][z][w] { // previously active
+							if n < 2 || n > 3 {
+								new[x][y][z][w] = false
+							}
+						} else { // previously inactive
+							if n == 3 {
+								new[x][y][z][w] = true
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	sum := 0
+	for x := range new {
+		for y := range new[x] {
+			for z := range new[x][y] {
+				for w := range new[x][y][z] {
+					if new[x][y][z][w] {
+						sum++
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("Part 2:", sum)
+}
