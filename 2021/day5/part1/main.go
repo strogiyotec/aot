7e3477c88722edc0165703d94027d6d924264eef
strogiyotec <almas337519@gmail.com>
2021-12-10 09:17:01 -0800

    start first part day 5
    Still unfinished, it gives 10 for small input instead of 5

---
+++ 2021/day5/part1/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type parts struct {
+	from int
+	to   int
+}
+
+func NewPart(line string) parts {
+	points := strings.Split(line, ",")
+	from, _ := strconv.Atoi(points[0])
+	to, _ := strconv.Atoi(points[1])
+	return parts{from: from, to: to}
+}
+
+func (part parts) similar(another parts) bool {
+	return part.from == another.from || part.to == another.to
+}
+
+func main() {
+	content, _ := ioutil.ReadFile("small_input.txt")
+	lines := strings.Split(string(content), "\n")
+	cnt := make(map[string]int)
+	for _, v := range lines {
+		parts := strings.Split(v, "->")
+		if len(parts) == 2 {
+			firstPart := NewPart(parts[0])
+			secondPart := NewPart(parts[1])
+			if firstPart.similar(secondPart) {
+				from := min(firstPart, secondPart)
+				to := max(firstPart, secondPart)
+				sameX := sameX(firstPart, secondPart)
+				for i := from; i <= to; i++ {
+					var element string
+					if sameX {
+						element = fmt.Sprintf("%d-%d", firstPart.from, i)
+					} else {
+						element = fmt.Sprintf("%d-%d", i, firstPart.to)
+					}
+					value, exist := cnt[element]
+					if !exist {
+						cnt[element] = 1
+					} else {
+						cnt[element] = value + 1
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(cnt)
+	totalPoints := 0
+	for _, v := range cnt {
+		if v >= 2 {
+			totalPoints++
+		}
+	}
+	fmt.Println(totalPoints)
+}
+
+func sameX(first, second parts) bool {
+	if first.from == second.from {
+		return true
+	}
+	return false
+}
+func max(first, second parts) int {
+	if first.to >= second.to {
+		return first.to
+	} else {
+		return second.to
+	}
+}
+func min(first, second parts) int {
+	if first.from <= second.from {
+		return first.from
+	} else {
+		return second.from
+	}
+}
